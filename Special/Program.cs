2b727ecfb554582119fa0bdcb066de488ab869eb
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-13 21:08:46 +0000

    create special method

--- Special/Program.cs
+++ Special/Program.cs
@@ -9,7 +9,19 @@ namespace Special
             Console.Write("Insert n-th number of the special sequence: ");
             int n = int.Parse(Console.ReadLine());
 
-            
+            int result = GetSpecial(n);
+
+            Console.WriteLine(result);
+        }
+        
+        private static int GetSpecial(int n)
+        {
+            if (n == 0) 
+                return 0;
+            if (n == 1) 
+                return 1;
+
+            return GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
         }
     }
 }
